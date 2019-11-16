2eeb176384d4c3b0cc3519ff2bdeb43dfc6d6380
sanchyy <vsanchez.gassull@gmail.com>
2019-11-16 19:21:28 +0100

    api

--- backend/api.go
+++ backend/api.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", home)
-	//http.HandleFunc("/newGroup", createGroup)
-	//http.HandleFunc("/update/{groupId}", updateGroup)
+	http.HandleFunc("/newGroup", createGroup)
+	http.HandleFunc("/update/{groupId}", updateGroup)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- backend/controller.go
+++ backend/controller.go
@@ -3,14 +3,23 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/sanchyy/ez_antibully/model"
 	"log"
 	"net/http"
 )
 
 type Response struct {
-	statusCode int               `json:"statusCode"`
-	headers    map[string]string `json:"headers"`
-	body       string            `json:"body"`
+	statusCode int    `json:"statusCode"`
+	body       string `json:"body"`
+}
+
+func register() {
+
+}
+
+func login() {
+
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +27,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 	log.Print("new connection :3")
 	res := &Response{
 		statusCode: 200,
-		headers:    map[string]string{"Content-Type": "application/json"},
 		body:       "Güelcom Jom",
 	}
 	_, err := json.Marshal(res)
@@ -29,9 +37,39 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func createGroup(w http.ResponseWriter, r *http.Request) {
+	//operations to create group on DB
 
+	res := &Response{
+		statusCode: func() {
+			if err != nil {
+				500
+			} else {
+				200
+			}
+		},
+		body: func() {
+			if err != nil {
+				fmt.sprintf("couldn't create group. Error: %s", err.Error())
+			} else {
+				"Group created successfully"
+			}
+		},
+	}
+	content, err := json.Marshal(res)
+	_, err := fmt.Fprintf(w, string(content))
+	if err != nil {
+		log.Printf(res.body)
+	}
 }
 
 func updateGroup() {
+
+}
+
+func getGroups() {
+
+}
+
+func addUser() {
 
 }
