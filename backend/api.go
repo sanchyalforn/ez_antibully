b714e9af8c9d625565e9553aa1b1846412925b9e
sanchyy <vsanchez.gassull@gmail.com>
2019-11-16 19:40:48 +0100

    all routes added

--- backend/api.go
+++ backend/api.go
@@ -10,6 +10,14 @@ import (
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/newGroup", createGroup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/getGroups/{userId}", getGroups)
+	http.HandleFunc("/addUser", addUser)
+	http.HandleFunc("/getUsers/{groupId}", getUsers)
+	http.HandleFunc("/getAnswers/{questionId", getAnswers)
+	http.HandleFunc("/getGroup/{groupId}", addUser)
 	http.HandleFunc("/update/{groupId}", updateGroup)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
